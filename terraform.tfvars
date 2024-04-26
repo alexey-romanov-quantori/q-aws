872f1e54aaa8845313288ada556019ef3d2b5f7c
Aleksei Romanov <alexey.romanov@quantori.com>
2024-04-26 20:14:33 +0100

    moved vars

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+subnet_id = "subnet-058c0197a05db2379"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-024cf058980b63412"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-07549c87757e073ea"
+}
